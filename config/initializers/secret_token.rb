44e957982966eb1a6fc424545f1ffba9c5ad2157
Taro Matsuzawa <taro@georepublic.co.jp>
2013-04-20 13:58:12 +0900

    init product

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dearmydebris::Application.config.secret_token = '9820c800d2092d5c6d6b11e64e519d34cbc9d97f782766e0292978c634e7aab3a335084cb4d766b428f2eb13f91362d228149324594ab8281323c758e827f4e0'
