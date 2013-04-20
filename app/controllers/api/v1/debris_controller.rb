8a1e67e4542b5ef3e6b97f10e34fc4cf9c828376
utiwa <Utiwa_6@yahoo.co.jp>
2013-04-20 18:20:52 +0900

    JSONのMockを作成しました。

--- app/controllers/api/v1/debris_controller.rb
+++ app/controllers/api/v1/debris_controller.rb
@@ -1,8 +1,23 @@
 class Api::V1::DebrisController < ApplicationController
   respond_to :json
   def index
-    res = { :latitude => 0, :longnitude => 90, :name => "hoge" }.as_json
-    respond_with res
+     res_geojson =
+    {
+      :type => "FeatureCollection",
+      :feature => [{
+        :type => "Feature",
+        :geometry => {
+          :type => "Point",
+          :coordinates => [ 10, 50, 10000 ]
+        },
+        :properties => {
+          :name => "VANGUARD",
+          :id => "hogehoge",
+          :follower => [ "osoken", "smellman" ]
+        }
+      }]
+    }.as_json
+    respond_with res_geojson
   end
 
   def show
